3579b1f7fa8b1bc6fa96ae8929002eb19e0be0f3
kai Feng <69415541+fengjiawei123@users.noreply.github.com>
2026-05-24 12:41:34 +0800

    Update test_MEF.py

--- test_MEF.py
+++ test_MEF.py
@@ -33,7 +33,7 @@ batch_size = 1
 GPU_number = os.environ['CUDA_VISIBLE_DEVICES']
 
 
-dataset = '/MFFW'
+dataset = 'MFFW/'
 # Model
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 Encoder = nn.DataParallel(Signal_Encoder()).to(device)
@@ -65,23 +65,16 @@ for i, (data_IR_y,data_IR_uv,data_VIS_y, data_VIS_uv,names) in enumerate(train_l
         signal_feature_IR, signal_ewt_IR, signal_mfb_IR = Encoder(data_IR)
         signal_feature_VIS, signal_ewt_VIS, signal_mfb_VIS = Encoder(data_VIS)
         data_Fuse = Fusion_Decoder(signal_feature_IR, signal_ewt_IR, signal_mfb_IR, signal_feature_VIS, signal_ewt_VIS, signal_mfb_VIS)
-        
-        
-        # if i %100 ==0:
-        path = "test_result/"+dataset
+        path = "Test_result/"+dataset
         if not os.path.exists(path):
             os.makedirs(path)
         for b in range(batch_size):
             data_Fuse_numpy = data_Fuse[b].cpu().detach().numpy().transpose(1, 2, 0) * 255
             tensor2 = (data_VIS_uv[b]+data_IR_uv[b]).squeeze(0).cpu().detach().numpy()/2.
-            
             YUV_image = np.concatenate((data_Fuse_numpy, tensor2), axis=2)
-            
             rgb_image_again = cv2.cvtColor(YUV_image, cv2.COLOR_YUV2RGB)
-            # data_Fuse_color = cv2.cvtColor(rgb_image_again, cv2.COLOR_RGB2BGR)
             data_Fuse_color = torch.Tensor(rgb_image_again).permute(2, 0, 1)/255.
-            # cv2.imwrite(os.path.join(path,f'/fusion{b}.png'), data_Fuse_color)
-            save_name = names[b]  # 原文件名（带后缀）
+            save_name = names[b]  
             save_path = os.path.join(path, save_name)
             torchvision.utils.save_image(data_Fuse_color, save_path)
             print("save:", save_path)
